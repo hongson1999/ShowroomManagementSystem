68637719d50a3b59bfbdd1e0a4f2f57c20fa3b8d
Hoang Phu <phunguyen01997@gmail.com>
2021-09-20 23:14:51 +0700

    Setup Interface Controller

---
+++ Server/Interfaces/IDealersController.cs
@@ -0,0 +1,23 @@
+﻿using Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Interfaces
+{
+    interface IDealersController
+    {
+        // Get
+        public IEnumerable<Dealer> Get();
+
+        // {id}
+        public Dealer Get(int id);
+
+        // Post
+        public bool Post(Dealer newDealer);
+
+        // Put
+        public bool Put(Dealer newDealer);
+    }
+}
---
+++ Server/Interfaces/IStatusOrdersController.cs
@@ -0,0 +1,14 @@
+﻿using Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Interfaces
+{
+    interface IStatusOrdersController
+    {
+        // Get
+        public IEnumerable<StatusOrder> Get();
+    }
+}
